5e9ca93af6399a4e1614671e42b67b168d42414e
Nicholas Keen <nicknack2019@yahoo.com>
2023-10-10 16:31:23 -0600

    corrected file structure for chamber

--- .history/lesson05/scripts/base_20231010162545.js
+++ .history/lesson05/scripts/base_20231010162545.js
@@ -1,10 +1,10 @@
 const input =document.querySelector('favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-button.addEventListener('click',function(){ if (input.focus() != " ") {} } );
+button.addEventListener('click',function(){ if (input.focus != " ") {} } );
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
-li.textContent = input.focus();
+li.textContent = input.value;
 deleteButton.textContent = '❌';
 li.append (deleteButton);
 list.append(li);
