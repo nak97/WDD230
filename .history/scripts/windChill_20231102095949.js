937f23c746cc8e8481746c53e476d938d836c791
Nicholas Keen <nicknack2019@yahoo.com>
2023-11-02 10:17:09 -0600

    changed info location

---
+++ .history/scripts/windChill_20231102095949.js
@@ -0,0 +1,10 @@
+const element = document.getElementById("weather-info");
+document.getElementById("temperature").innerHTML = "Temperature: 100 F";
+document.getElementById("wind-speed").innerHTML = "wind speed: 10 mph";
+
+
+
+const windChill = 35.74 + 0.6215 * 100 - 35.75 * (10**0.16)
+
+
+document.getElementById("wind-chill").textContent = windChill;
